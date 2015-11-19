fea15503619cc74c65497bd51669a71161daef13
Shoaib0597 <Shoaib0595@gmail.com>
2015-11-19 20:16:01 +0530

    Updated PowerSuspend to v1.5
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>
    Signed-off-by: TheImpulson <aayushgupta219@gmail.com>
    
     Conflicts:
    	kernel/power/autosleep.c

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -14,6 +14,11 @@
 #include <linux/workqueue.h>
 #include <linux/module.h>
 //[---]Debug for active wakelock before entering suspend
+
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "power.h"
 
 static suspend_state_t autosleep_state;
@@ -124,13 +129,23 @@ int pm_autosleep_set_state(suspend_state_t state)
         mod_timer(&unattended_timer, jiffies + msecs_to_jiffies(PM_UNATTENDED_TIMEOUT));
 		//[---]Debug for active wakelock before entering suspend
 		queue_up_suspend_work();
+#ifdef CONFIG_POWERSUSPEND
+		// Yank555.lu : add hook to handle powersuspend tasks (sleep)
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
+#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
+
 		//[+++]Debug for active wakelock before entering suspend
 		//Add a timer to trigger wakelock debug
         pr_info("[PM]unattended_timer: del_timer (late_resume)\n");
         del_timer(&unattended_timer);
 		//[---]Debug for active wakelock before entering suspend
+
+#ifdef CONFIG_POWERSUSPEND
+		// Yank555.lu : add hook to handle powersuspend tasks (wakeup)
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE);
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
@@ -179,3 +194,4 @@ int __init pm_autosleep_init(void)
 	wakeup_source_unregister(autosleep_ws);
 	return -ENOMEM;
 }
+
