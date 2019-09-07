61048ca630fe2d7282d2f9d18547868b2dbb1f55
theimpulson <aayushgupta219@gmail.com>
2019-09-07 08:01:58 +0000

    FK: r3.1

--- build_Z00L.sh
+++ build_Z00L.sh
@@ -10,7 +10,7 @@ nocol='\033[0m'
 
 # Kernel details
 KERNEL_NAME="FireKernel"
-VERSION="r3"
+VERSION="r3.1"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="Z00L"
 FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE-$DEVICE.zip
--- build_Z00T.sh
+++ build_Z00T.sh
@@ -10,7 +10,7 @@ nocol='\033[0m'
 
 # Kernel details
 KERNEL_NAME="FireKernel"
-VERSION="r3"
+VERSION="r3.1"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="Z00T"
 FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE-$DEVICE.zip
